0b3641be1b16dfc745727727603ae533654edbaf
ashimiblessing <ashimiblessing@hotmail.com>
2020-07-30 08:42:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,35 @@
 
   
   
-  function chinko_maker()
+  function chinko_maker(event)
   {
-    
+    event.preventDefault();
     var sourceText= document.getElementById('your_name').value;
     
-  
-  
-  function doGet(e) {
- 
-    if (e.parameter.q) {
-        sourceText = e.parameter.q;
-    }
-
-    var sourceLang = 'auto';
-    if (e.parameter.source) {
-        sourceLang = e.parameter.source;
-    }
-
-    var targetLang = 'ja';
-    if (e.parameter.target) {
-        targetLang = e.parameter.target;
-    } /* Option 1 */
-    var translatedText = LanguageApp.translate(sourceText, sourceLang, targetLang) /* Option 2 */ var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
-    var result = JSON.parse(UrlFetchApp.fetch(url).getContentText());
-    translatedText = result[0][0][0];
-    var json = {
-        'sourceText': sourceText,
-        'translatedText': translatedText
-    }; 
+  doGet(sourceText);
     
-    // set JSONP callback 
-    var callback = 'callback';   
-    if(e.parameter.callback){     callback = e.parameter.callback   }   
-    // return JSONP   
-    return ContentService.createTextOutput(callback + '(' + JSON.stringify(json) + ')').setMimeType(ContentService.MimeType.JAVASCRIPT);
+}
+
+  function doGet(sourceText) {
+ 
+   
+    var sourceLang = 'auto';
+    var targetLang = 'ja';
+ 
+    /* Option 2 */ 
+    
+    var url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=" + sourceLang + "&tl=" + targetLang + "&dt=t&q=" + encodeURI(sourceText);
+    var result = JSON.parse(UrlFetchApp.fetch(url).getContentText());
+    
+    $.get( url, function( data ) {
+       var result = JSON.parse(data);
+    var  translatedText = result[0][0][0];
+      alert(translatedText);
+ // $( ".result" ).html( data );
+   
+});
+   
 }
   
   
-  
-}
+
